b7285ed34e88fc6b63f430d308ca198db1101649
1263919792@qq.com <1w2b3c4g>
2021-09-05 23:55:26 +0800

    elasticsearch

---
+++ kafka-demo/kafka-consumer/src/main/java/com/study/xc/KafkaConsumerDemo.java
@@ -0,0 +1,39 @@
+package com.study.xc;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConsumerDemo {
+
+    // broker集合
+    private static final String brokerList = "localhost:9092";
+    // 主题名称
+    private static final String topic = "topic-demo";
+
+    // 消费者组名称
+    private static final String groupId = "group.demo";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        // 消费者客户端
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        // 订阅主题
+        consumer.subscribe(Collections.singleton(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            records.forEach(record -> System.out.println(record.value()));
+        }
+    }
+}
