679f1777e1af9691457900d474a6275541b4b455
1263919792@qq.com <1w2b3c4g>
2021-09-06 23:43:09 +0800

    kafka

--- kafka-demo/kafka-consumer/src/main/java/com/study/xc/KafkaConsumerDemo.java
+++ kafka-demo/kafka-consumer/src/main/java/com/study/xc/KafkaConsumerDemo.java
@@ -3,10 +3,14 @@ package com.study.xc;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 public class KafkaConsumerDemo {
@@ -15,7 +19,6 @@ public class KafkaConsumerDemo {
     private static final String brokerList = "localhost:9092";
     // 主题名称
     private static final String topic = "topic-demo";
-
     // 消费者组名称
     private static final String groupId = "group.demo";
 
@@ -31,6 +34,11 @@ public class KafkaConsumerDemo {
         // 订阅主题
         consumer.subscribe(Collections.singleton(topic));
 
+        // 订阅指定的分区
+        consumer.assign(Collections.singletonList(new TopicPartition(topic, 0)));
+
+        // 获取指定主题的分区信息
+        List<PartitionInfo> partitionInfos = consumer.partitionsFor(topic);
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
             records.forEach(record -> System.out.println(record.value()));
