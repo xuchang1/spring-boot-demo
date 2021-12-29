a1a7abc8445a750cc63e4d57aa7ed4dc8e17562a
1263919792@qq.com <1w2b3c4g>
2021-12-29 23:47:39 +0800

    shiro

---
+++ shiro-demo/src/main/java/com/xc/study/entity/Permissions.java
@@ -0,0 +1,11 @@
+package com.xc.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Permissions {
+    private String id;
+    private String permissionsName;
+}
---
+++ shiro-demo/src/main/java/com/xc/study/entity/User.java
@@ -0,0 +1,19 @@
+package com.xc.study.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String id;
+    private String userName;
+    private String password;
+    /**
+     * 用户对应的角色集合
+     */
+    private Set<Role> roles;
+}
